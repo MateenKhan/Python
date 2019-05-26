ab4d23a6a814a8215c5336e82e800d431f2f4717
Mateen Khan <makjavaprogrammer@gmail.com>
2019-05-27 05:07:23 +0530

    basic calculator program! updated added exception handling

--- calculator.py
+++ calculator.py
@@ -1,29 +1,54 @@
-userInput = "";
-print("select the arithmetic operation to perform");
-print("1.Addition \n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Power");
-print("type 'quit' to exit");
-while userInput!='quit':
-    userInput = input("operation:")
-    if userInput == 'quit':
-        break;
-    firstNumber = int(input("first number:"))
-    secondNumber = int(input("first number:"))
-    if userInput == "1":
-        print(firstNumber+secondNumber);
-    elif userInput == "2":
-        print(firstNumber - secondNumber);
-    elif userInput == "3":
-        print(firstNumber * secondNumber);
-    elif userInput == "4":
-        print(firstNumber / secondNumber);
-    elif userInput == "5":
-        print(firstNumber % secondNumber);
-    elif userInput == "6":
-        print(firstNumber ** secondNumber);
-    else:
-        print("invalid input, try again!!")
-        break;
+def print_info():
+    print("\n\ntype 'quit' to exit\n\n")
+    print("select the arithmetic operation to perform")
+    print("1.Addition \t2.Subtraction\t3.Multiplication\t4.Division\t5.Modulus\t6.Power")
 
 
 
+def get_user_input():
+    user_input = input("operation:");
+    return user_input
 
+
+def is_valid_user_input(user_input):
+    try:
+        user_selection = int(user_input)
+        if user_selection < 1 or user_selection > 6:
+            print("\n\nInvalid option selected try again!!")
+            return False
+    except ValueError:
+        print("\n\nInvalid option selected try again!!")
+        return False
+    return True
+
+
+def calculator(user_input):
+    while user_input != 'quit':
+        user_input = get_user_input()
+        if user_input == 'quit':
+            break
+        if not is_valid_user_input(user_input):
+            print_info()
+            continue
+
+        first_number = int(input("first number:"))
+        second_number = int(input("first number:"))
+        if user_input == "1":
+            print(first_number+second_number)
+        elif user_input == "2":
+            print(first_number - second_number)
+        elif user_input == "3":
+            print(first_number * second_number)
+        elif user_input == "4":
+            print(first_number / second_number)
+        elif user_input == "5":
+            print(first_number % second_number)
+        elif user_input == "6":
+            print(first_number ** second_number)
+        else:
+            print("invalid input, try again!!")
+            break
+
+
+print_info()
+calculator(user_input="")
